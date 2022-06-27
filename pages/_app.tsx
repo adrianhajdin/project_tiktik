5eb815defd7182be31317b967703f1f5fe707645
adrianhajdin <adrianhajdin00@gmail.com>
2022-06-28 00:10:42 +0200

    update envs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
       <Navbar />
-      <div className='flex gap-6 md:gap-20 '>
+       <div className='flex gap-6 md:gap-20 '>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
           <Sidebar />
         </div>
