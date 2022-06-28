e87bfa2f7e32a5d92ddb578473f30dde10f28d5b
adrianhajdin <adrianhajdin00@gmail.com>
2022-06-28 10:21:33 +0200

    ...

--- utils/index.ts
+++ utils/index.ts
@@ -3,16 +3,16 @@ import axios from 'axios';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const fetchGoogleResponse = async (response: any, addUser: any) => {
-  addUser(response?.profileObj);
-    
-  const user = { 
-    _id: response?.profileObj?.googleId, 
-    _type: 'user', 
-    userName: response?.profileObj?.name, 
-    image: response?.profileObj?.imageUrl 
-  };
+  const { name, googleId, imageUrl } = response.profileObj;
   
-  if(response?.profileObj) {
-    await axios.post(`${BASE_URL}/api/auth`, user);
-  }
+  addUser(response.profileObj);
+
+  const user = {
+    _id: googleId,
+    _type: 'user',
+    userName: name,
+    image: imageUrl,
+  };
+
+  await axios.post(`${BASE_URL}/api/auth`, user);
 };
