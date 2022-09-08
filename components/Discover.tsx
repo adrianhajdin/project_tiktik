8cf587e105fda8e4506aabfd81618c4c84cd001a
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-09-09 00:48:53 +0800

    if you having an error about this query use this

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -7,7 +7,8 @@ import { topics } from '../utils/constants';
 
 const Discover: NextPage = () => {
   const router = useRouter();
-  const { topic } = router.query;
+//   const { topic } = router.query;
+  const topic = router.asPath.split('/')[2];
 
   const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
   const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
